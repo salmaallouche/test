0c2d9d94a03e360a283e894b09183a52e8ac5a61
metoui <metoui@MacBook-Pro-de-metoui.local>
2016-02-09 21:23:47 +0100

    Adam

---
+++ adam.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  before_filter :set_language
+  helper_method :current_company
+  helper_method :current_product_types
+  helper_method :allowed_segments
+  helper_method :current_segment
+  helper_method :current_cart
